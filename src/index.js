0e28f2bc23b8a1d53d56ec30cf88f0f15effa7db
dane hague <danehague1@googlemail.com>
2017-05-03 12:59:18 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import numeral from 'numeral';
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 
+
+render(
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
