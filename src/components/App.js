c7c9d9b5a783a4197bcbc4fe92ea6fcb6713b0eb
Dane <danehague1@googlemail.com>
2018-01-06 12:36:01 +0000

    fixed a few things

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends React.Component {
     render() {
         return(
             <div>
-                <p>herader</p>
+                <p>Header</p>
                 {this.props.children}
             </div>
         );
